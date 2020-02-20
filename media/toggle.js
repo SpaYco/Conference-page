51e55efb562234fe22e2a92cb90c4956687963eb
SpaYco <zizou.aziz101@gmail.com>
2020-02-20 09:17:53 +0100

    updated toggle.js for better performance

--- media/toggle.js
+++ media/toggle.js
@@ -2,14 +2,14 @@ function toggleBtn() {
     let navLinks = document.getElementsByClassName('toggle');
     let btn = document.getElementById('toggle-btn');
     if (window.getComputedStyle(navLinks[0]).opacity == 0) {
+        btn.style.transform = "rotate(90deg)";
         for (i = 0; i < navLinks.length; i++){
             navLinks[i].style.opacity = 1;
-            btn.style.transform = "rotate(90deg)";
         }
     } else {
+        btn.style.transform = "rotate(0deg)";
         for (i = 0; i < navLinks.length; i++){
             navLinks[i].style.opacity = 0;
-            btn.style.transform = "rotate(0deg)";
         }
     }
 }
